97bfb7d17bb3412d180c0af990958bc92723706d
Stefan M. <StefMa@users.noreply.github.com>
2024-08-27 07:01:56 +0000

    Use Uuid from common lib

--- library/src/commonMain/kotlin/com/ioki/passenger/api/internal/utils/Expects.kt
+++ library/src/commonMain/kotlin/com/ioki/passenger/api/internal/utils/Expects.kt
@@ -5,5 +5,3 @@ import kotlinx.datetime.LocalDateTime
 internal expect fun parseRfc1123DateTime(dateTimeString: String): LocalDateTime
 
 internal expect val Throwable.isConnectivityError: Boolean
-
-internal expect fun randomUUID(): String
