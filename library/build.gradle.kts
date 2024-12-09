61a2cd160155c436947ebebd469537fcd0b6555d
StefMa <StefMaDev@outlook.com>
2024-12-09 10:26:01 +0100

    Set JVM version to 17

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
@@ -15,12 +13,11 @@ kotlin {
 
     applyDefaultHierarchyTemplate()
 
+    jvmToolchain(17)
+
     jvm()
     androidTarget {
         publishLibraryVariants("release")
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_1_8)
-        }
     }
     iosX64()
     iosArm64()
