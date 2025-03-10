b5b6395a30085b4aa6654fa6590b89eb9004cd85
Stefan M. <StefMa@users.noreply.github.com>
2025-03-10 16:25:18 +0100

    Update ApiRideFilterType.kt

--- library/src/commonMain/kotlin/com/ioki/passenger/api/models/ApiRideFilterType.kt
+++ library/src/commonMain/kotlin/com/ioki/passenger/api/models/ApiRideFilterType.kt
@@ -4,5 +4,5 @@ public enum class ApiRideFilterType(public val queryValue: String) {
     ACTIVE("active"),
     FINISHED_OR_CAUSED_FEE("finished_or_caused_fee"),
     FINISHED_OR_CAUSED_FEE_OR_CANCELLED_BY_OPERATOR("finished_or_caused_fee_or_cancelled_by_operator"),
-    FAILED_PAYMENTS("payment_failed"),
+    PAYMENT_FAILED("payment_failed"),
 }
