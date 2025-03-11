f8c8c8246776fcf52793c229b5c5343d2cbf8f4c
Stefan M. <StefMa@users.noreply.github.com>
2025-03-11 13:40:30 +0100

    Add new RideFilterType (#113)

--- library/src/commonMain/kotlin/com/ioki/passenger/api/models/ApiRideFilterType.kt
+++ library/src/commonMain/kotlin/com/ioki/passenger/api/models/ApiRideFilterType.kt
@@ -2,6 +2,7 @@ package com.ioki.passenger.api.models
 
 public enum class ApiRideFilterType(public val queryValue: String) {
     ACTIVE("active"),
-    PAST("finished_or_caused_fee"),
-    FAILED_PAYMENTS("payment_failed"),
+    FINISHED_OR_CAUSED_FEE("finished_or_caused_fee"),
+    FINISHED_OR_CAUSED_FEE_OR_CANCELLED_BY_OPERATOR("finished_or_caused_fee_or_cancelled_by_operator"),
+    PAYMENT_FAILED("payment_failed"),
 }
