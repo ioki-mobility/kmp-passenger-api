570b57b8435349bad4258b39aeee924ad58c6847
StefMa <StefMaDev@outlook.com>
2025-05-14 12:51:27 +0200

    Use nmcp plugin to publish to central portal

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,20 @@
 plugins {
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
+    alias(libs.plugins.nmcpAggregation)
 }
 
-subprojects.forEach {
+allprojects.forEach {
     it.group = "com.ioki"
     it.version = "0.9.0-SNAPSHOT"
 }
 
+nmcpAggregation {
+    centralPortal {
+        username = System.getenv("SONATYPE_USER")
+        password = System.getenv("SONATYPE_PASSWORD")
+        publishingType = "USER_MANAGED"
+    }
+
+    publishAllProjectsProbablyBreakingProjectIsolation()
+}
