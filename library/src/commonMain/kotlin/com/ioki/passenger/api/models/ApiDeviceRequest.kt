4880ed4247fac8faed40ac5a763000adf39290f0
Julian Schmitt <juschmitt91@gmail.com>
2025-07-21 10:14:12 +0200

    fix issues

--- library/src/commonMain/kotlin/com/ioki/passenger/api/models/ApiDeviceRequest.kt
+++ library/src/commonMain/kotlin/com/ioki/passenger/api/models/ApiDeviceRequest.kt
@@ -1,9 +1,10 @@
 package com.ioki.passenger.api.models
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 public data class ApiDeviceRequest(
     val token: String,
-    @SerialName(value = supports_encrypted_notifications) val supportsEncryptedNotifications: Boolean,
+    @SerialName(value = "supports_encrypted_notifications") val supportsEncryptedNotifications: Boolean,
 )
