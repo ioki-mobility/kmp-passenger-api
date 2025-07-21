6ecf455c13580086d31a28706d2561b0c73d1851
Julian Schmitt <juschmitt91@gmail.com>
2025-07-21 10:05:56 +0200

    adjust api model

--- library/src/commonMain/kotlin/com/ioki/passenger/api/models/ApiDeviceRequest.kt
+++ library/src/commonMain/kotlin/com/ioki/passenger/api/models/ApiDeviceRequest.kt
@@ -5,4 +5,5 @@ import kotlinx.serialization.Serializable
 @Serializable
 public data class ApiDeviceRequest(
     val token: String,
+    @SerialName(value = supports_encrypted_notifications) val supportsEncryptedNotifications: Boolean,
 )
