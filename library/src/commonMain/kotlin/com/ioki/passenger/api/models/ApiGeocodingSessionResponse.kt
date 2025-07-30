ae8e378c5f8d7ad658257a82f45c38a0532920b1
elmira.yafteh@ioki.com <elmira.yafteh@ioki.com>
2025-07-30 19:55:52 +0200

    Add ApiGeocodingSessionResponse

---
+++ library/src/commonMain/kotlin/com/ioki/passenger/api/models/ApiGeocodingSessionResponse.kt
@@ -0,0 +1,13 @@
+package com.ioki.passenger.api.models
+
+import kotlinx.datetime.Instant
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+public data class ApiGeocodingSessionResponse(
+    val id: String,
+    @SerialName(value = "created_at") val createdAt: Instant,
+    @SerialName(value = "updated_at") val updatedAt: Instant?,
+    @SerialName(value = "valid_until") val validUntil: Instant,
+)
