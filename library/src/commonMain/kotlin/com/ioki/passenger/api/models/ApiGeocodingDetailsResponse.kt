bab9bf5dd41cdf1c9e375cedb83932384aa4844f
elmira.yafteh@ioki.com <elmira.yafteh@ioki.com>
2025-07-30 19:54:56 +0200

    Add ApiGeocodingDetailsResponse

---
+++ library/src/commonMain/kotlin/com/ioki/passenger/api/models/ApiGeocodingDetailsResponse.kt
@@ -0,0 +1,33 @@
+package com.ioki.passenger.api.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+public data class ApiGeocodingDetailsResponse(
+    val id: String,
+    val lat: Double,
+    val lng: Double,
+    val vendor: String,
+    @SerialName(value = "vendor_id") val vendorId: String,
+    @SerialName(value = "vendor_type") val vendorType: VendorType,
+    @SerialName(value = "location_name") val locationName: String?,
+    @SerialName(value = "formatted_address") val formattedAddress: String?,
+    @SerialName(value = "street_name") val streetName: String?,
+    @SerialName(value = "street_number") val streetNumber: String?,
+    @SerialName(value = "postal_code") val postalCode: String?,
+    val city: String?,
+    val county: String?,
+    val country: String?,
+) {
+    @Serializable
+    public enum class VendorType {
+        @SerialName("station")
+        STATION,
+
+        @SerialName("places")
+        PLACES,
+
+        UNSUPPORTED,
+    }
+}
---
+++ library/src/commonTest/kotlin/com/ioki/passenger/api/models/ApiGeocodingDetailsResponseTest.kt
@@ -0,0 +1,84 @@
+package com.ioki.passenger.api.models
+
+import com.ioki.passenger.api.models.ApiGeocodingDetailsResponse.VendorType
+import kotlin.test.Test
+
+internal class ApiGeocodingDetailsResponseTest : IokiApiModelTest() {
+    @Test
+    fun serialization() {
+        testJsonStringCanBeConvertedToModel(
+            expectedModel = ApiGeocodingDetailsResponse(
+                id = "abc123",
+                lat = 50.113695,
+                lng = 8.678996,
+                vendorType = VendorType.PLACES,
+                locationName = "An der Welle",
+                vendor = "ioki",
+                vendorId = "vendor_123",
+                formattedAddress = "An der Welle 3, Frankfurt am Main, Germany",
+                city = "Frankfurt am Main",
+                county = "Hessen",
+                country = "Germany",
+                postalCode = "60322",
+                streetName = "An der Welle",
+                streetNumber = "3",
+            ),
+            jsonString = details,
+        )
+    }
+
+    @Test
+    fun serializationMinimal() {
+        testJsonStringCanBeConvertedToModel(
+            expectedModel = ApiGeocodingDetailsResponse(
+                id = "abc123",
+                lat = 50.113695,
+                lng = 8.678996,
+                vendor = "ioki",
+                vendorId = "vendor_123",
+                vendorType = VendorType.PLACES,
+                locationName = null,
+                formattedAddress = null,
+                city = null,
+                county = null,
+                country = null,
+                postalCode = null,
+                streetName = null,
+                streetNumber = null,
+            ),
+            jsonString = detailsMinimal,
+        )
+    }
+}
+
+private val details =
+    """
+{
+    "id": "abc123",
+    "lat": 50.113695,
+    "lng": 8.678996,
+    "vendor": "ioki",
+    "vendor_id": "vendor_123",
+    "vendor_type": "places",
+    "location_name": "An der Welle",
+    "formatted_address": "An der Welle 3, Frankfurt am Main, Germany",
+    "city": "Frankfurt am Main",
+    "county": "Hessen",
+    "country": "Germany",
+    "postal_code": "60322",
+    "street_name": "An der Welle",
+    "street_number": "3"
+}
+"""
+
+private val detailsMinimal =
+    """
+{
+    "id": "abc123",
+    "lat": 50.113695,
+    "lng": 8.678996,
+    "vendor": "ioki",
+    "vendor_id": "vendor_123",
+    "vendor_type": "places"
+}
+"""
