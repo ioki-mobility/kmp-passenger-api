79759e2c3f87499733f715668a79607e59590755
elmira-y <104195450+elmira-y@users.noreply.github.com>
2025-10-13 11:21:50 +0200

    ApiPersonalDiscountResponse: Add new channel enums (#206)

--- library/src/commonMain/kotlin/com/ioki/passenger/api/models/ApiPersonalDiscountResponse.kt
+++ library/src/commonMain/kotlin/com/ioki/passenger/api/models/ApiPersonalDiscountResponse.kt
@@ -1,8 +1,15 @@
 package com.ioki.passenger.api.models
 
+import com.ioki.passenger.api.models.ApiPersonalDiscountResponse.Channel
+import com.ioki.passenger.api.models.ApiPersonalDiscountResponse.DiscountType
+import com.ioki.passenger.api.models.ApiPersonalDiscountResponse.Validity
 import kotlin.time.Instant
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 
 @Serializable
 public data class ApiPersonalDiscountResponse(
@@ -31,7 +38,7 @@ public data class ApiPersonalDiscountResponse(
     @SerialName(value = "redeemed_promo_code_id")
     val redeemedPromoCodeId: String?,
 ) {
-    @Serializable
+    @Serializable(ApiPersonalDiscountResponseChannelSerializer::class)
     public enum class Channel {
         @SerialName(value = "purchased")
         PURCHASED,
@@ -44,10 +51,26 @@ public data class ApiPersonalDiscountResponse(
 
         @SerialName(value = "operator_assigned")
         OPERATOR_ASSIGNED,
+
+        @SerialName(value = "operator_assigned_legacy")
+        OPERATOR_ASSIGNED_LEGACY,
+
+        @SerialName(value = "assigned_for_good_will")
+        ASSIGNED_FOR_GOOD_WILL,
+
+        @SerialName(value = "assigned_for_promotion")
+        ASSIGNED_FOR_PROMOTION,
+
+        @SerialName(value = "assigned_for_other_reason")
+        ASSIGNED_FOR_OTHER_REASON,
+
+        @SerialName(value = "sold_by_operator")
+        SOLD_BY_OPERATOR,
+
         UNSUPPORTED,
     }
 
-    @Serializable
+    @Serializable(ApiPersonalDiscountResponseDiscountTypeSerializer::class)
     public enum class DiscountType {
         @SerialName(value = "absolute")
         ABSOLUTE,
@@ -63,7 +86,7 @@ public data class ApiPersonalDiscountResponse(
         UNSUPPORTED,
     }
 
-    @Serializable
+    @Serializable(ApiPersonalDiscountResponseValiditySerializer::class)
     public enum class Validity {
         @SerialName(value = "usage")
         USAGE,
@@ -74,5 +97,85 @@ public data class ApiPersonalDiscountResponse(
         @SerialName(value = "usage_and_time")
         USAGE_AND_TIME,
         UNSUPPORTED,
+    }
+}
+
+private object ApiPersonalDiscountResponseChannelSerializer : KSerializer<Channel> {
+    override val descriptor = String.serializer().descriptor
+
+    override fun serialize(encoder: Encoder, value: Channel) {
+        encoder.encodeString(
+            when (value) {
+                Channel.PURCHASED -> "purchased"
+                Channel.PROMOTION -> "promotion"
+                Channel.REFERRAL -> "referral"
+                Channel.OPERATOR_ASSIGNED -> "operator_assigned"
+                Channel.OPERATOR_ASSIGNED_LEGACY -> "operator_assigned_legacy"
+                Channel.ASSIGNED_FOR_GOOD_WILL -> "assigned_for_good_will"
+                Channel.ASSIGNED_FOR_PROMOTION -> "assigned_for_promotion"
+                Channel.ASSIGNED_FOR_OTHER_REASON -> "assigned_for_other_reason"
+                Channel.SOLD_BY_OPERATOR -> "sold_by_operator"
+                Channel.UNSUPPORTED -> "unsupported"
+            },
+        )
+    }
+
+    override fun deserialize(decoder: Decoder): Channel = when (decoder.decodeString()) {
+        "purchased" -> Channel.PURCHASED
+        "promotion" -> Channel.PROMOTION
+        "referral" -> Channel.REFERRAL
+        "operator_assigned" -> Channel.OPERATOR_ASSIGNED
+        "operator_assigned_legacy" -> Channel.OPERATOR_ASSIGNED_LEGACY
+        "assigned_for_good_will" -> Channel.ASSIGNED_FOR_GOOD_WILL
+        "assigned_for_promotion" -> Channel.ASSIGNED_FOR_PROMOTION
+        "assigned_for_other_reason" -> Channel.ASSIGNED_FOR_OTHER_REASON
+        "sold_by_operator" -> Channel.SOLD_BY_OPERATOR
+        else -> Channel.UNSUPPORTED
+    }
+}
+
+private object ApiPersonalDiscountResponseDiscountTypeSerializer : KSerializer<DiscountType> {
+    override val descriptor = String.serializer().descriptor
+
+    override fun serialize(encoder: Encoder, value: DiscountType) {
+        encoder.encodeString(
+            when (value) {
+                DiscountType.ABSOLUTE -> "absolute"
+                DiscountType.RELATIVE -> "relative"
+                DiscountType.FIXED_PER_PASSENGER -> "fixed_per_passenger"
+                DiscountType.FIXED_PER_RIDE -> "fixed_per_ride"
+                DiscountType.UNSUPPORTED -> "unsupported"
+            },
+        )
+    }
+
+    override fun deserialize(decoder: Decoder): DiscountType = when (decoder.decodeString()) {
+        "absolute" -> DiscountType.ABSOLUTE
+        "relative" -> DiscountType.RELATIVE
+        "fixed_per_passenger" -> DiscountType.FIXED_PER_PASSENGER
+        "fixed_per_ride" -> DiscountType.FIXED_PER_RIDE
+        else -> DiscountType.UNSUPPORTED
+    }
+}
+
+private object ApiPersonalDiscountResponseValiditySerializer : KSerializer<Validity> {
+    override val descriptor = String.serializer().descriptor
+
+    override fun serialize(encoder: Encoder, value: Validity) {
+        encoder.encodeString(
+            when (value) {
+                Validity.USAGE -> "usage"
+                Validity.TIME -> "time"
+                Validity.USAGE_AND_TIME -> "usage_and_time"
+                Validity.UNSUPPORTED -> "unsupported"
+            },
+        )
+    }
+
+    override fun deserialize(decoder: Decoder): Validity = when (decoder.decodeString()) {
+        "usage" -> Validity.USAGE
+        "time" -> Validity.TIME
+        "usage_and_time" -> Validity.USAGE_AND_TIME
+        else -> Validity.UNSUPPORTED
     }
 }
