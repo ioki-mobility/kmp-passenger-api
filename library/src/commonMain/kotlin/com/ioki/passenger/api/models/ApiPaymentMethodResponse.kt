baa2f316d5ce323217e427afe9a5696d5665db38
elmira.yafteh@ioki.com <elmira.yafteh@ioki.com>
2025-10-21 10:54:58 +0200

    ApiPaymentMethodResponse: Add EXTERNAL_PAYMENT kind, ApiPaymentMethodResponseKindSerializer

--- library/src/commonMain/kotlin/com/ioki/passenger/api/models/ApiPaymentMethodResponse.kt
+++ library/src/commonMain/kotlin/com/ioki/passenger/api/models/ApiPaymentMethodResponse.kt
@@ -1,7 +1,12 @@
 package com.ioki.passenger.api.models
 
+import com.ioki.passenger.api.models.ApiPaymentMethodResponse.Summary.Kind
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 
 @Serializable
 public data class ApiPaymentMethodResponse(
@@ -18,7 +23,7 @@ public data class ApiPaymentMethodResponse(
         @SerialName(value = "mandate_url")
         val mandateUrl: String?,
     ) {
-        @Serializable
+        @Serializable(with = ApiPaymentMethodResponseKindSerializer::class)
         public enum class Kind {
             @SerialName(value = "card")
             CREDIT_CARD,
@@ -37,7 +42,43 @@ public data class ApiPaymentMethodResponse(
 
             @SerialName(value = "pos_payment")
             POS_PAYMENT,
+
+            @SerialName(value = "external_payment")
+            EXTERNAL_PAYMENT,
+
             UNSUPPORTED,
+        }
+    }
+}
+
+internal object ApiPaymentMethodResponseKindSerializer : KSerializer<Kind> {
+    override val descriptor = String.serializer().descriptor
+
+    override fun serialize(encoder: Encoder, value: Kind) {
+        encoder.encodeString(
+            when (value) {
+                Kind.CREDIT_CARD -> "card"
+                Kind.SEPA_DEBIT -> "sepa_debit"
+                Kind.PAYPAL -> "paypal"
+                Kind.CASH -> "cash"
+                Kind.SERVICE_CREDITS -> "service_credits"
+                Kind.POS_PAYMENT -> "pos_payment"
+                Kind.EXTERNAL_PAYMENT -> "external_payment"
+                Kind.UNSUPPORTED -> "unsupported"
+            },
+        )
+    }
+
+    override fun deserialize(decoder: Decoder): Kind {
+        return when (decoder.decodeString()) {
+            "card" -> Kind.CREDIT_CARD
+            "sepa_debit" -> Kind.SEPA_DEBIT
+            "paypal" -> Kind.PAYPAL
+            "cash" -> Kind.CASH
+            "service_credits" -> Kind.SERVICE_CREDITS
+            "pos_payment" -> Kind.POS_PAYMENT
+            "external_payment" -> Kind.EXTERNAL_PAYMENT
+            else -> Kind.UNSUPPORTED
         }
     }
 }
