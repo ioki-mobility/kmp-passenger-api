2c2381b3265c38428f7ba29c7288687a664be7fb
StefMa <StefMaDev@outlook.com>
2025-10-22 11:23:18 +0200

    Fix ApiMatchingState

--- library/src/commonMain/kotlin/com/ioki/passenger/api/models/ApiMatchingState.kt
+++ library/src/commonMain/kotlin/com/ioki/passenger/api/models/ApiMatchingState.kt
@@ -3,12 +3,7 @@ package com.ioki.passenger.api.models
 import kotlinx.serialization.Serializable
 
 @Serializable
-public data class ApiMatchingState(
-    val id: String,
-    val logs: List<MatchingStateLog>,
-) {
+public data class ApiMatchingState(val id: String, val logs: List<MatchingStateLog>) {
     @Serializable
-    public data class MatchingStateLog(
-        val message: String,
-    )
+    public data class MatchingStateLog(val message: String)
 }
