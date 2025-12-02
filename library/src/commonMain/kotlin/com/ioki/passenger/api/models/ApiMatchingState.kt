a8acb3d78aaef2c1ce148687c3209f7ddf130ea8
Julian Schmitt <juschmitt91@gmail.com>
2025-12-02 13:48:58 +0100

    fix lint again

--- library/src/commonMain/kotlin/com/ioki/passenger/api/models/ApiMatchingState.kt
+++ library/src/commonMain/kotlin/com/ioki/passenger/api/models/ApiMatchingState.kt
@@ -3,11 +3,7 @@ package com.ioki.passenger.api.models
 import kotlinx.serialization.Serializable
 
 @Serializable
-public data class ApiMatchingState(
-    val id: String,
-    val logs: List<MatchingStateLog>,
-    val finalSummary: String?,
-) {
+public data class ApiMatchingState(val id: String, val logs: List<MatchingStateLog>, val finalSummary: String?) {
     @Serializable
     public data class MatchingStateLog(val message: String)
 }
