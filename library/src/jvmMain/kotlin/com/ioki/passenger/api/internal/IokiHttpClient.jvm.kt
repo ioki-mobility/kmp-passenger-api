eab022f2890add19f35313851293f31656b5f96a
StefMa <StefMaDev@outlook.com>
2026-02-23 13:42:42 +0100

    Share jvm and android target

--- library/src/jvmMain/kotlin/com/ioki/passenger/api/internal/IokiHttpClient.jvm.kt
+++
@@ -1,6 +0,0 @@
-package com.ioki.passenger.api.internal
-
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.okhttp.OkHttp
-
-internal actual fun httpClient(): HttpClient = HttpClient(OkHttp)
