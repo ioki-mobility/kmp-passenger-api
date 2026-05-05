3cbf916ce5c37a2684305912b650658a50c7eb00
StefMa <StefMaDev@outlook.com>
2026-05-05 10:12:29 +0200

    Next development version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 allprojects.forEach { project ->
     project.group = "com.ioki"
     project.version = buildString {
-        append("0.24.0")
+        append("0.25.0")
         project.findProperty("PRE_RELEASE_IDENTIFIER")?.let {
             append('-')
             append(it)
