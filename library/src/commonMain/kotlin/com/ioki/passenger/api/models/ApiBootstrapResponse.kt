7fc1be0c07f3b5ee6a6c41a494d1ebfd2a253606
elmira.yafteh@ioki.com <elmira.yafteh@ioki.com>
2026-06-29 13:21:18 +0200

    ApiBootstrapResponse: Add PaymentServiceProvider

--- library/src/commonMain/kotlin/com/ioki/passenger/api/models/ApiBootstrapResponse.kt
+++ library/src/commonMain/kotlin/com/ioki/passenger/api/models/ApiBootstrapResponse.kt
@@ -1,13 +1,19 @@
 package com.ioki.passenger.api.models
 
+import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonContentPolymorphicSerializer
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 
 @Serializable
 public data class ApiBootstrapResponse(
     val provider: ApiProvider,
     val products: List<ApiProduct>,
     val client: ApiClientInfoResponse,
+    @SerialName(value = "payment_service_provider") val paymentServiceProvider: PaymentServiceProvider?,
     @SerialName(value = "rides_with_failed_payments") val ridesWithFailedPayments: List<ApiRideResponse>,
     @SerialName(value = "renewable_ticketing_vouchers")
     val renewableTicketingVouchers: List<ApiTicketingVoucherResponse>,
@@ -18,4 +24,48 @@ public data class ApiBootstrapResponse(
         @SerialName(value = "product_id") val productId: String,
         @SerialName(value = "support_website_uri") val supportWebsiteUri: String,
     )
+}
+
+internal object PaymentServiceProviderSerializer : JsonContentPolymorphicSerializer<PaymentServiceProvider>(
+    PaymentServiceProvider::class,
+) {
+    override fun selectDeserializer(element: JsonElement): DeserializationStrategy<PaymentServiceProvider> {
+        val jsonObject = element.jsonObject
+        val type = jsonObject.getValue("type").jsonPrimitive.content
+        return when (type) {
+            "payment_service_provider/stripe" -> PaymentServiceProvider.Stripe.serializer()
+            "payment_service_provider/logpay" -> PaymentServiceProvider.LogPay.serializer()
+            else -> throw IllegalArgumentException("Unsupported PaymentServiceProvider type.")
+        }
+    }
+}
+
+@Serializable(PaymentServiceProviderSerializer::class)
+public sealed class PaymentServiceProvider {
+    public abstract val type: Type
+
+    @Serializable
+    public data class Stripe(
+        @SerialName(value = "stripe_account_id") val stripeAccountId: String?,
+        @SerialName(value = "stripe_payment_method_types") val stripeTypes: List<ApiStripeType>?,
+        @SerialName(value = "google_pay_supported") val googlePaySupported: Boolean,
+    ) : PaymentServiceProvider() {
+        override val type: Type = Type.STRIPE
+    }
+
+    @Serializable
+    public data class LogPay(
+        @SerialName(value = "logpay_payment_method_types") val logPayTypes: List<ApiLogPayType>?,
+    ) : PaymentServiceProvider() {
+        override val type: Type = Type.LOGPAY
+    }
+
+    @Serializable
+    public enum class Type {
+        @SerialName(value = "payment_service_provider/logpay")
+        LOGPAY,
+
+        @SerialName(value = "payment_service_provider/stripe")
+        STRIPE,
+    }
 }
--- test/src/commonMain/kotlin/com/ioki/passenger/api/test/models/ApiBootstrapResponse.kt
+++ test/src/commonMain/kotlin/com/ioki/passenger/api/test/models/ApiBootstrapResponse.kt
@@ -2,15 +2,19 @@ package com.ioki.passenger.api.test.models
 
 import com.ioki.passenger.api.models.ApiBootstrapResponse
 import com.ioki.passenger.api.models.ApiClientInfoResponse
+import com.ioki.passenger.api.models.ApiLogPayType
 import com.ioki.passenger.api.models.ApiProduct
 import com.ioki.passenger.api.models.ApiProvider
 import com.ioki.passenger.api.models.ApiRideResponse
+import com.ioki.passenger.api.models.ApiStripeType
 import com.ioki.passenger.api.models.ApiTicketingVoucherResponse
+import com.ioki.passenger.api.models.PaymentServiceProvider
 
 public fun createApiBootstrapResponse(
     provider: ApiProvider = createApiProvider(),
     products: List<ApiProduct> = emptyList(),
     client: ApiClientInfoResponse = createApiClientInfoResponse(),
+    paymentServiceProvider: PaymentServiceProvider? = null,
     ridesWithFailedPayments: List<ApiRideResponse> = emptyList(),
     renewableTicketingVouchers: List<ApiTicketingVoucherResponse> = emptyList(),
     productSupportUris: List<ApiBootstrapResponse.ProductSupportUri> = emptyList(),
@@ -18,7 +22,21 @@ public fun createApiBootstrapResponse(
     provider = provider,
     products = products,
     client = client,
+    paymentServiceProvider = paymentServiceProvider,
     ridesWithFailedPayments = ridesWithFailedPayments,
     renewableTicketingVouchers = renewableTicketingVouchers,
     productSupportUris = productSupportUris,
 )
+
+public fun createStripePaymentServiceProvider(
+    stripeAccountId: String? = null,
+    stripeTypes: List<ApiStripeType>? = null,
+    googlePaySupported: Boolean = false,
+): PaymentServiceProvider = PaymentServiceProvider.Stripe(
+    googlePaySupported = googlePaySupported,
+    stripeTypes = stripeTypes,
+    stripeAccountId = stripeAccountId,
+)
+
+public fun createLogPayPaymentServiceProvider(logPayTypes: List<ApiLogPayType>? = null): PaymentServiceProvider =
+    PaymentServiceProvider.LogPay(logPayTypes = logPayTypes)
